06400852e99a478f82d4a833c4046a793dbb9914
Omkar More <114797074+Omkar1344@users.noreply.github.com>
2025-04-01 17:35:14 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
-export const MONGODB_URI="mongodb+srv://itsoml1304:Omkar%401344@cluster0.4dvdjye.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Cluster0"
+export const MONGODB_URI="Your URL"
 // export const MONGODB_URI="mongodb://localhost:27017/bookstore";
